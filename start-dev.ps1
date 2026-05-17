463b44762e1280327e415583823103eb1e9101c9
Gregory Luna <gregluna4809@gmail.com>
2026-05-17 17:52:40 -0400

    Remove local development startup scripts

--- start-dev.ps1
+++
@@ -1,5 +0,0 @@
-$root = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$root\backend'; mvn spring-boot:run"
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$root\frontend'; npm run dev"
